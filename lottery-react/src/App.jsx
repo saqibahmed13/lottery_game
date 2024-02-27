a197cdd3c8069851c45bc8bfed19ce739c9b7c65
saqibahmed13 <saqiba0484@gmail.com>
2024-02-27 20:17:07 +0530

    first commit

---
+++ lottery-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Lottery from './Lottery.jsx'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Lottery/>
+      </div>
+    </>
+  )
+}
+
+export default App
