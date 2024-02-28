baa0c8a6b55df2b1624899cb0b952280a2c32838
saqibahmed13 <saqiba0484@gmail.com>
2024-02-28 22:10:40 +0530

    add

--- lottery-react/src/helper.js
+++ lottery-react/src/helper.js
@@ -1,7 +1,7 @@
 function genTicket(n){
     let arr = new Array(n);
     for(let i=0;i<n;i++){
-        arr[i] = Math.floor(Math.random()*10);  // this will generate values 0 - 9  thats why *10
+        arr[i] = Math.floor(Math.random()*10);   // this will generate values 0 - 9  thats why *10
     }
     return arr;
 }
