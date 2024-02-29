ac2f451a3e0cd539ef0bd64a8b41bdeda8d2df64
saqibahmed13 <saqiba0484@gmail.com>
2024-02-29 15:00:06 +0530

    added

--- lottery-react/src/Lottery.jsx
+++ lottery-react/src/Lottery.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./Lottery.css";
-import { genTicket } from "./helper.js";
+import { genTicket, sum } from "./helper.js";
 
 export default function Lottery() {
   const [ticket, setticket] = useState(genTicket(3));
+  console.log("me", ticket);
+  let isWinning = sum(ticket) === 15;
+
+  let buyTicket = ()=>{
+    setticket(genTicket(3));
+  }
   return (
     <div>
       <h1>Lottery Game!</h1>
@@ -12,6 +18,8 @@ export default function Lottery() {
         <span>{ticket[1]}</span>
         <span>{ticket[2]}</span>
       </div>
+      <button onClick={buyTicket}>Buy Ticket</button>
+      <h3>{isWinning && "Congratulations you won"}</h3>
     </div>
   );
 }
